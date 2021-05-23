8c93440a1a9a062c30c7172f093c56bb876e2fd9
a.bashkatov <as@bashkatov.pro>
2021-05-23 22:59:27 +0300

    adds security libraries

---
+++ src/main/java/pro/bashkatov/pokerapi/config/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package pro.bashkatov.pokerapi.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .formLogin().disable()
+                .logout().disable()
+        ;
+    }
+}
